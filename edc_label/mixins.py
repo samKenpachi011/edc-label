35f99f019f4a2be18c2d6e1fcceddeaff0d43475
erikvw <ew2789@gmail.com>
2016-09-07 07:14:56 +0200

    split view mixin methods to separate mixin

---
+++ edc_label/mixins.py
@@ -0,0 +1,44 @@
+from edc_label.label import app_config, Label
+from edc_label.print_server import PrintServer
+
+
+class EdcLabelMixin:
+
+    print_server_error = None
+
+    def __init__(self, **kwargs):
+        self._print_server = None
+        self._printers = {}
+        self.cups_server_ip = app_config.default_cups_server_ip
+        self.printer_label = app_config.default_printer_label
+        super(EdcLabelMixin, self).__init__(**kwargs)
+
+    @property
+    def print_server(self):
+        if not self._print_server:
+            if self.cups_server_ip:
+                self._print_server = PrintServer(self.cups_server_ip)
+            else:
+                self._print_server = PrintServer()
+            self._print_server.select_printer(self.printer_label)
+        return self._print_server
+
+    @property
+    def printers(self):
+        if not self._printers:
+            if self.print_server:
+                for printer in self.print_server.printers.items():
+                    printer = str(printer[0])
+                    printer_properties = {k.replace('-', '_'): v for k, v in self.print_server.printers[printer].items()}
+                    self._printers.update({printer: printer_properties})
+        return self._printers
+
+    def print_label(self, label_name, copies=None, context=None):
+        copies = 3 if copies is None else copies
+        label_template = app_config.label_templates.get(label_name)
+        context = label_template.test_context if context is None else context
+        label = Label(label_name, print_server=self.print_server, context=context)
+        label.print_label(copies)
+        return label
+
+
--- edc_label/view_mixins.py
+++ edc_label/view_mixins.py
@@ -2,20 +2,11 @@ import json
 
 from django.http.response import HttpResponse
 
-from edc_label.label import app_config, Label
-from edc_label.print_server import PrintServer
+from edc_label.label import app_config
+from edc_label.mixins import EdcLabelMixin
 
 
-class EdcLabelViewMixin:
-
-    print_server_error = None
-
-    def __init__(self, **kwargs):
-        self._print_server = None
-        self._printers = {}
-        self.cups_server_ip = app_config.default_cups_server_ip
-        self.printer_label = app_config.default_printer_label
-        super(EdcLabelViewMixin, self).__init__(**kwargs)
+class EdcLabelViewMixin(EdcLabelMixin):
 
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
@@ -48,31 +39,3 @@ class EdcLabelViewMixin:
             'default_cups_server_ip': app_config.default_cups_server_ip or 'localhost',
         })
         return context
-
-    @property
-    def print_server(self):
-        if not self._print_server:
-            if self.cups_server_ip:
-                self._print_server = PrintServer(self.cups_server_ip)
-            else:
-                self._print_server = PrintServer()
-            self._print_server.select_printer(self.printer_label)
-        return self._print_server
-
-    @property
-    def printers(self):
-        if not self._printers:
-            if self.print_server:
-                for printer in self.print_server.printers.items():
-                    printer = str(printer[0])
-                    printer_properties = {k.replace('-', '_'): v for k, v in self.print_server.printers[printer].items()}
-                    self._printers.update({printer: printer_properties})
-        return self._printers
-
-    def print_label(self, label_name, copies=None, context=None):
-        copies = 3 if copies is None else copies
-        label_template = app_config.label_templates.get(label_name)
-        context = label_template.test_context if context is None else context
-        label = Label(label_name, print_server=self.print_server, context=context)
-        label.print_label(copies)
-        return label
